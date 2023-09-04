156a793a9c972775e461f628be1afbaaf8e8409f
JuanLucca846 <lucca470@gmail.com>
2023-09-03 22:18:35 -0300

    pilares

---
+++ Pilares/src/Carro.java
@@ -0,0 +1,20 @@
+public class Carro {
+    private String chassi;
+    public String getChassi(){
+        return chassi;
+    }
+    public void setChassi(String chassi){
+        this.chassi = chassi;
+    }
+    public void ligar(){
+        confereCambio();
+        confereCombustivel();
+        System.out.println("Carro Ligado");
+    }
+    private void confereCombustivel(){
+        System.out.println("Conferindo combustivel");
+    }
+    private void confereCambio(){
+        System.out.println("Conferinfo cambio");
+    }
+}
